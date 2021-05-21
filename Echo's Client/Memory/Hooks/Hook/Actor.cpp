522e98db25b28e12ee51c920e9251d8fe9b50cab
EchoHackCmd <EchoHackCmd@gmail.com>
2021-05-21 12:52:21 +0100

    Update Actor.cpp

--- Echo's Client/Memory/Hooks/Hook/Actor.cpp
+++ Echo's Client/Memory/Hooks/Hook/Actor.cpp
@@ -2,7 +2,7 @@
 #include "../../../Client/Client.h"
 
 void Actor_Hook::init() {
-	Utils::DebugLogF("Preparing to hook onto the Actor VTables");
+	//Utils::DebugLogF("Preparing to hook onto the Actor VTables");
 	uintptr_t sigAddr = Utils::FindSig("48 8D 05 ? ? ? ? 48 89 01 48 83 C1 08 48 89 4C 24 ? 33 D2");
 	if (!sigAddr) return;
 	
