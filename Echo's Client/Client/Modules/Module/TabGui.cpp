d7a17dcc3c9df660fb2efc07b66188f79060758c
EchoHackCmd <EchoHackCmd@gmail.com>
2021-05-22 06:41:27 +0100

    Update TabGui.cpp

--- Echo's Client/Client/Modules/Module/TabGui.cpp
+++ Echo's Client/Client/Modules/Module/TabGui.cpp
@@ -1,9 +1,58 @@
 #include "TabGui.h"
 
+class TC { /* Text Component */
+public:
+	int ID = 0;
+	float cOffX = 0;
+	float cOffY = 0;
+
+	TC(int ID, Vec2 offPos) {
+		this->ID = ID;
+		this->cOffX = offPos.x;
+		this->cOffY = offPos.y;
+	}
+
+	void shiftX(float newX, float multiplyr = 0.5f) {
+		if (this->cOffX != newX) {
+			if (this->cOffX < newX) {
+				this->cOffX += multiplyr;
+			}
+			else if (this->cOffX > newX) {
+				this->cOffX -= multiplyr;
+			}
+		}
+	}
+};
+
+std::vector<TC*> textComponents = std::vector<TC*>();
+
 void TabGui::onRender(class Renderer* renderer) {
 	if (instance == nullptr || instance->guiData() == nullptr) return;
 
-	//
+	int i = 0;
+	float tSize = instance->guiData()->GuiScale() * 10;
+
+	for (auto C : client->categories) {
+		Vec2 tPos = Vec2(10, (i * (tSize + 5)) + 10);
+
+		static bool reg = false;
+		if (!reg) {
+			textComponents.push_back(new TC(i, tPos));
+			if(i == (client->categories.size() - 1)) reg = true;
+		}
+
+		auto cTC = textComponents.at(i);
+
+		if (sCat && sCIndex == i) {
+			cTC->shiftX(tPos.x + 10);
+		}
+		else {
+			cTC->shiftX(tPos.x);
+		}
+
+		renderer->drawString(std::wstring(C->name.begin(), C->name.end()), tSize, Vec2(tPos.x + cTC->cOffX, tPos.y), _RGBA(52, 152, 219, 1));
+		i++;
+	}
 }
 
 void TabGui::onKey(uint64_t key, bool isDown, bool* cancelOrigin) {
@@ -28,7 +77,11 @@ void TabGui::onKey(uint64_t key, bool isDown, bool* cancelOrigin) {
 		}
 		if (key == 0x25) { /* Left Arrow */
 			*cancelOrigin = true;
-			if (sCat) {
+			if (sMod) {
+				sMod = false;
+				sMIndex = 0;
+			}
+			else if (sCat) {
 				sCat = false;
 				sCIndex = 0;
 			}
