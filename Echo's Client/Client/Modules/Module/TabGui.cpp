5c379339f6f0ab1413d713293ae3478d6fcaa4bd
EchoHackCmd <EchoHackCmd@gmail.com>
2021-05-22 03:45:46 +0100

    Update TabGui.cpp

--- Echo's Client/Client/Modules/Module/TabGui.cpp
+++ Echo's Client/Client/Modules/Module/TabGui.cpp
@@ -3,21 +3,7 @@
 void TabGui::onRender(class Renderer* renderer) {
 	if (instance == nullptr || instance->guiData() == nullptr) return;
 
-	size_t cIndex = 0;
-	Vec2 res = instance->guiData()->res;
-	float textSize = instance->guiData()->GuiScale() * 10;
-
-	std::wstring logo = std::wstring(client->name.begin(), client->name.end());
-
-	renderer->drawString(logo, textSize, Vec2(res.x - ((logo.length() + 1) * (textSize / 2)), res.y - (textSize * 2)), _RGBA(57, 126, 216));
-
-	for (auto C : client->categories) {
-		Vec2 textPos = Vec2(15, (cIndex * (textSize + 5)) + 15);
-		renderer->drawString(std::wstring(C->name.begin(), C->name.end()), textSize, (sCat ? sCIndex == cIndex ? Vec2(textPos.x + 10, textPos.y) : textPos : textPos), _RGBA(57, 126, 216));
-		cIndex++;
-	}
-
-	//renderer->fillRectangle(Vec2(50, 100), Vec2(300, 300), _RGBA(57, 126, 216, 0.6f));
+	//
 }
 
 void TabGui::onKey(uint64_t key, bool isDown, bool* cancelOrigin) {
