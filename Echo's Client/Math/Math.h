76f62b86ea0e890b775407d88190228143beee94
EchoHackCmd <EchoHackCmd@gmail.com>
2021-06-01 05:53:31 +0100

    Removed no longer needed FW1FontWrapper RGBA Function I made

--- Echo's Client/Math/Math.h
+++ Echo's Client/Math/Math.h
@@ -136,25 +136,3 @@ public:
 		this->a = a;
 	}
 };
-
-class RGBA_FW1 {
-public:
-	float r;
-	float g;
-	float b;
-	float a;
-
-	RGBA_FW1(float r, float g, float b, float a = 255) {
-		//this->r = r; this->g = b; this->b = b; this->a = a;
-		this->b = r;
-		this->g = g;
-		this->r = b;
-	}
-	UINT32 toUint32() {
-		UINT32 _r = 255 * r;
-		UINT32 _b = 255 * b;
-		UINT32 _g = 255 * g;
-
-		return 0xFF000000 | _r | (_b << 16) | (_g << 8);
-	}
-};
