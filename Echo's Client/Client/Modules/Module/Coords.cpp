b86c1ebb59776d01ed9786fc6e187138948b9422
EchoHackCmd <EchoHackCmd@gmail.com>
2021-06-01 12:36:17 +0100

    Update Coords.cpp

--- Echo's Client/Client/Modules/Module/Coords.cpp
+++ Echo's Client/Client/Modules/Module/Coords.cpp
@@ -1,8 +1,21 @@
 #include "Coords.h"
 
+void Coords::updateAlpha() {
+	float modifier = 0.03f;
+	MinecraftGame* game = instance->minecraftGame();
+	if (game->canUseKeys) {
+		if (textColour.a < 1.0f) textColour.a += modifier;
+	}
+	else {
+		if (textColour.a > 0) textColour.a -= modifier;
+	}
+}
+
 void Coords::onRender(class Renderer* renderer) {
 	if (player == nullptr || instance == nullptr || instance->guiData() == nullptr)
 		return;
+
+	updateAlpha();
 	
 	auto pos = *player->getPos();
 	auto size = instance->guiData()->GuiScale() * 10;
@@ -13,7 +26,7 @@ void Coords::onRender(class Renderer* renderer) {
 	float len = renderer->textWidth(t, size);
 
 	Vec2 res = instance->guiData()->res;
-	Vec2 tPos = Vec2(4, res.y - (renderer->textHeight(t, size) + (instance->guiData()->GuiScale() * 8)));
+	Vec2 tPos = Vec2(4, res.y - (renderer->textHeight(t, size) + (instance->guiData()->GuiScale() * 6)));
 
-	renderer->drawString(t, size, tPos, _RGBA(255, 255, 255));
+	renderer->drawString(t, size, tPos, textColour);
 }
