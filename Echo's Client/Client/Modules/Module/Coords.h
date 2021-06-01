e99c2ad5345dbb4c89e5f07c0b3525208887f3cc
EchoHackCmd <EchoHackCmd@gmail.com>
2021-06-01 12:36:20 +0100

    Update Coords.h

--- Echo's Client/Client/Modules/Module/Coords.h
+++ Echo's Client/Client/Modules/Module/Coords.h
@@ -5,4 +5,7 @@ class Coords : public Module {
 public:
 	Coords(class Client* client, class Category* category, std::string name) : Module(client, category, name) {};
 	void onRender(class Renderer*);
+	void updateAlpha();
+
+	_RGBA textColour = _RGBA(255, 255, 255, 1.0f);
 };
